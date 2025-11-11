23e87e099812034273d0d2b189564e97b822b11d
Ruben Rocha <ruben@evolvedigitaldesign.com>
2025-11-10 19:53:58 -0700

    fix: admin portal formatting

--- RegulAgentV2/regulagent-backend/ra_config/settings/base.py
+++ RegulAgentV2/regulagent-backend/ra_config/settings/base.py
@@ -116,7 +116,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
